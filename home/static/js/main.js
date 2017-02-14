b440223e2eef6215c5ac5cce075b74bb2643481c
imhub <doroshe79@gmail.com>
2017-02-14 11:43:43 +0200

    datepicker added

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -1,9 +1,19 @@
+$("#topContainer").css("min-height",$(window).height());
+$("#newsContainer").css("min-height",$(window).height());
+$("#bottomContainer").css("min-height",$(window).height());
 
-  $("#topContainer").css("min-height",$(window).height());
-  $("#newsContainer").css("min-height",$(window).height());
-  $("#bottomContainer").css("min-height",$(window).height());
+function message(msg, tag) {
+  		document.getElementById(tag).innerHTML = msg;
+    }
 
-  function message(msg, tag)
-  	{
-    		document.getElementById(tag).innerHTML = msg;
-	}
+function initDateFields() {
+      $('input.dateinput').datetimepicker({
+        'format': 'YYYY-MM-DD'
+      }).on('dp.hide', function(event){
+        $(this).blur();
+      });
+    }
+
+$(document).ready(function(){
+      initDateFields();
+});
